e00af983f177f72d712ff3950ce30017afa4a5e4
LuluEmc2 <lucien.vite@proton.me>
2024-09-15 14:51:26 +0200

    Added getters using tuples.

--- Model/Model.cpp
+++ Model/Model.cpp
@@ -1,11 +1,35 @@
-#include <list>
-#include <unordered_map>
 #include "Model.hpp"
 
 using namespace std;
 
 Model::Model(){}
 
+tuple<string, string, float> Model::GetDocument(string _documentId)
+{
+    if(m_documents[_documentId] == nullptr)
+    {
+        return {"", "", 0.0f};
+    }
+
+    string title = m_documents[_documentId]->GetTitle();
+    string description = m_documents[_documentId]->GetDescription();
+    float length = m_documents[_documentId]->GetLength();
+
+    return {title, description, length};
+}
+
+tuple<string, string> Model::GetContainer(string _containerId)
+{
+    if(m_containers[_containerId] == nullptr)
+    {
+        return {"", ""};
+    }
+
+    string name = m_containers[_containerId]->GetName();
+    string description = m_containers[_containerId]->GetDescription();
+
+    return {name, description};
+}
 
 unordered_set<string> Model::GetDocuments(string _containerId)
 {
--- Model/Model.hpp
+++ Model/Model.hpp
@@ -3,6 +3,7 @@
 
 #include <unordered_set>
 #include <unordered_map>
+#include <tuple>
 #include "Document.hpp"
 #include "Container.hpp"
 
@@ -13,7 +14,13 @@ class Model
     public:
         Model();
 
+        //Returns a tuple of title, description, length, returns {"", "", 0.0f} if Id is invalid
+        tuple<string, string, float> GetDocument(string _documentId);
+        //Returns a tuple of name, description, returns {"", ""} if Id is invalid
+        tuple<string, string> GetContainer(string _containerId);
+        //Returns a set of the documents in container Ids
         unordered_set<string> GetDocuments(string _containerId);
+        //Returns a set of the containers of document Ids
         unordered_set<string> GetContainers(string _documentId);
 
         //Returns 1 if there's already a doc with the same Id, 0 else
