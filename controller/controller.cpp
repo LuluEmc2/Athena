a5091d5dfc2c89159f08b3e83229eb6a4f63eba8
LuluEmc2 <lucien.vite@proton.me>
2024-09-27 20:38:46 +0200

    Added controller namespace.

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,145 +1,146 @@
 #include "controller.hpp"
 
-using namespace std;
-
-Controller::Controller(){}
-
-tuple<string, string, string, double> Controller::GetDocument(string _documentId)
+namespace controller
 {
-    if(m_documents[_documentId] == nullptr)
+    Controller::Controller(){}
+
+    std::tuple<std::string, std::string, std::string, double> Controller::GetDocument(std::string _documentId)
     {
-        return {NO_FOUND_ERROR, "", "", 0.0f};
+        if(m_documents[_documentId] == nullptr)
+        {
+            return {NO_FOUND_ERROR, "", "", 0.0f};
+        }
+
+        std::string title = m_documents[_documentId]->GetTitle();
+        std::string description = m_documents[_documentId]->GetDescription();
+        double length = m_documents[_documentId]->GetLength();
+
+        return {_documentId, title, description, length};
     }
 
-    string title = m_documents[_documentId]->GetTitle();
-    string description = m_documents[_documentId]->GetDescription();
-    double length = m_documents[_documentId]->GetLength();
-
-    return {_documentId, title, description, length};
-}
-
-tuple<string, string, string> Controller::GetContainer(string _containerId)
-{
-    if(m_containers[_containerId] == nullptr)
+    std::tuple<std::string, std::string, std::string> Controller::GetContainer(std::string _containerId)
     {
-        return {NO_FOUND_ERROR, "", ""};
+        if(m_containers[_containerId] == nullptr)
+        {
+            return {NO_FOUND_ERROR, "", ""};
+        }
+
+        std::string name = m_containers[_containerId]->GetName();
+        std::string description = m_containers[_containerId]->GetDescription();
+
+        return {_containerId, name, description};
     }
 
-    string name = m_containers[_containerId]->GetName();
-    string description = m_containers[_containerId]->GetDescription();
-
-    return {_containerId, name, description};
-}
-
-unordered_set<string> Controller::GetDocuments(string _containerId)
-{
-    return m_containersToDocuments[_containerId];
-}
-
-unordered_set<string> Controller::GetContainers(string _documentId)
-{
-    return m_documentsToContainers[_documentId];
-}
-
-
-int Controller::AddDocument(string _id, string _title, string _description, double _length)
-{
-    if(m_documents[_id] != nullptr || m_containers[_id] != nullptr)
+    std::unordered_set<std::string> Controller::GetDocuments(std::string _containerId)
     {
-        return 1;
+        return m_containersToDocuments[_containerId];
     }
 
-    if(_id == NO_FOUND_ERROR)
+    std::unordered_set<std::string> Controller::GetContainers(std::string _documentId)
     {
-        return -1;
+        return m_documentsToContainers[_documentId];
     }
 
-    m_documents[_id] = new Document(_title, _description, _length);
 
-    return 0;
-}
-
-int Controller::RemoveDocument(string _id)
-{
-    if(m_documents[_id] == nullptr)
+    int Controller::AddDocument(std::string _id, std::string _title, std::string _description, double _length)
     {
-        return 1;
+        if(m_documents[_id] != nullptr || m_containers[_id] != nullptr)
+        {
+            return 1;
+        }
+
+        if(_id == NO_FOUND_ERROR)
+        {
+            return -1;
+        }
+
+        m_documents[_id] = new document::Document(_title, _description, _length);
+
+        return 0;
     }
 
-    delete m_documents[_id];
-
-    for (auto& container : GetContainers(_id))
+    int Controller::RemoveDocument(std::string _id)
     {
-        m_containersToDocuments[container].erase(_id);
+        if(m_documents[_id] == nullptr)
+        {
+            return 1;
+        }
+
+        delete m_documents[_id];
+
+        for (auto& container : GetContainers(_id))
+        {
+            m_containersToDocuments[container].erase(_id);
+        }
+
+        m_documentsToContainers.erase(_id);
+
+        m_documents.erase(_id);
+
+        return 0;
     }
 
-    m_documentsToContainers.erase(_id);
-
-    m_documents.erase(_id);
-
-    return 0;
-}
-
-int Controller::AddContainer(string _id, string _title, string _description)
-{
-    if(m_documents[_id] != nullptr || m_containers[_id] != nullptr)
+    int Controller::AddContainer(std::string _id, std::string _title, std::string _description)
     {
-        return 1;
+        if(m_documents[_id] != nullptr || m_containers[_id] != nullptr)
+        {
+            return 1;
+        }
+
+        m_containers[_id] = new container::Container(_title, _description);
+
+        return 0;
     }
 
-    m_containers[_id] = new Container(_title, _description);
-
-    return 0;
-}
-
-int Controller::RemoveContainer(string _id)
-{
-    if(m_containers[_id] == nullptr)
+    int Controller::RemoveContainer(std::string _id)
     {
-        return 1;
+        if(m_containers[_id] == nullptr)
+        {
+            return 1;
+        }
+
+        delete m_containers[_id];
+
+        for (auto& document : GetDocuments(_id))
+        {
+            m_documentsToContainers[document].erase(_id);
+        }
+
+        m_containersToDocuments.erase(_id);
+
+        m_containers.erase(_id);
+
+        return 0;
     }
 
-    delete m_containers[_id];
-
-    for (auto& document : GetDocuments(_id))
+    int Controller::StoreDocument(std::string _documentId, std::string _containerId)
     {
-        m_documentsToContainers[document].erase(_id);
+        if(m_documents[_documentId] == nullptr || m_containers[_containerId] == nullptr)
+        {
+            return 1;
+        }
+
+        m_documentsToContainers[_documentId].insert(_containerId);
+        m_containersToDocuments[_containerId].insert(_documentId);
+
+        return 0;
     }
 
-    m_containersToDocuments.erase(_id);
-
-    m_containers.erase(_id);
-
-    return 0;
-}
-
-int Controller::StoreDocument(string _documentId, string _containerId)
-{
-    if(m_documents[_documentId] == nullptr || m_containers[_containerId] == nullptr)
+    int Controller::UnstoreDocument(std::string _documentId, std::string _containerId)
     {
-        return 1;
+        if(m_documents[_documentId] == nullptr || m_containers[_containerId] == nullptr)
+        {
+            return 1;
+        }
+
+        if(m_containersToDocuments[_containerId].count(_documentId) == 0)
+        {
+            return -1;
+        }
+
+        m_documentsToContainers[_documentId].erase(_containerId);
+        m_containersToDocuments[_containerId].erase(_documentId);
+
+        return 0;
     }
-
-    m_documentsToContainers[_documentId].insert(_containerId);
-    m_containersToDocuments[_containerId].insert(_documentId);
-
-    return 0;
-}
-
-int Controller::UnstoreDocument(string _documentId, string _containerId)
-{
-    if(m_documents[_documentId] == nullptr || m_containers[_containerId] == nullptr)
-    {
-        return 1;
-    }
-
-    if(m_containersToDocuments[_containerId].count(_documentId) == 0)
-    {
-        return -1;
-    }
-
-    m_documentsToContainers[_documentId].erase(_containerId);
-    m_containersToDocuments[_containerId].erase(_documentId);
-
-    return 0;
 }
--- controller/controller.hpp
+++ controller/controller.hpp
@@ -9,44 +9,45 @@
 
 #define NO_FOUND_ERROR "__NF__"
 
-using namespace std;
-
-class Controller
+namespace controller
 {
-    public:
-        Controller();
+    class Controller
+    {
+        public:
+            Controller();
 
-        //Returns a tuple of id, title, description, length, returns {UNFOUND_ERROR, "", "", 0.0f} if Id is invalid
-        tuple<string, string, string, double> GetDocument(string _documentId);
-        //Returns a tuple of id, name, description, returns {UNFOUND_ERROR, "", ""} if Id is invalid
-        tuple<string, string, string> GetContainer(string _containerId);
-        //Returns a set of the documents in container Ids
-        unordered_set<string> GetDocuments(string _containerId);
-        //Returns a set of the containers of document Ids
-        unordered_set<string> GetContainers(string _documentId);
+            //Returns a tuple of id, title, description, length, returns {UNFOUND_ERROR, "", "", 0.0f} if Id is invalid
+            std::tuple<std::string, std::string, std::string, double> GetDocument(std::string _documentId);
+            //Returns a tuple of id, name, description, returns {UNFOUND_ERROR, "", ""} if Id is invalid
+            std::tuple<std::string, std::string, std::string> GetContainer(std::string _containerId);
+            //Returns a set of the documents in container Ids
+            std::unordered_set<std::string> GetDocuments(std::string _containerId);
+            //Returns a set of the containers of document Ids
+            std::unordered_set<std::string> GetContainers(std::string _documentId);
 
-        //Returns 1 if there's already a doc or a container with the same Id, -1 if it uses a keyword, 0 else
-        int AddDocument(string _id, string _title, string _description, double _length);
-        //Returns 1 if there isn't a doc with this Id, 0 else
-        int RemoveDocument(string _id);
-        //Returns 1 if there's already a container or a doc with the same Id, -1 if it uses a keyword, 0 else
-        int AddContainer(string _id, string _title, string _description);
-        //Returns 1 if there isn't a container with this Id, 0 else
-        int RemoveContainer(string _id);
+            //Returns 1 if there's already a doc or a container with the same Id, -1 if it uses a keyword, 0 else
+            int AddDocument(std::string _id, std::string _title, std::string _description, double _length);
+            //Returns 1 if there isn't a doc with this Id, 0 else
+            int RemoveDocument(std::string _id);
+            //Returns 1 if there's already a container or a doc with the same Id, -1 if it uses a keyword, 0 else
+            int AddContainer(std::string _id, std::string _title, std::string _description);
+            //Returns 1 if there isn't a container with this Id, 0 else
+            int RemoveContainer(std::string _id);
 
-        //Returns 1 if the container or the document doesn't exists, 0 else
-        int StoreDocument(string _documentId, string _containerId);
-        //Returns 1 if the container or the document doesn't exists, -1 if the links between them doesn't exists, 0 else
-        int UnstoreDocument(string _documentId, string _containerId);
-        
-    private:
-        //Links the Id with the objects
-        unordered_map<string, Document*> m_documents;
-        unordered_map<string,Container*> m_containers;
+            //Returns 1 if the container or the document doesn't exists, 0 else
+            int StoreDocument(std::string _documentId, std::string _containerId);
+            //Returns 1 if the container or the document doesn't exists, -1 if the links between them doesn't exists, 0 else
+            int UnstoreDocument(std::string _documentId, std::string _containerId);
 
-        //Parents documents and containers using ids.
-        unordered_map<string, unordered_set<string>> m_documentsToContainers;
-        unordered_map<string, unordered_set<string>> m_containersToDocuments;
-};
+        private:
+            //Links the Id with the objects
+            std::unordered_map<std::string, document::Document*> m_documents;
+            std::unordered_map<std::string, container::Container*> m_containers;
+
+            //Parents documents and containers using ids.
+            std::unordered_map<std::string, std::unordered_set<std::string>> m_documentsToContainers;
+            std::unordered_map<std::string, std::unordered_set<std::string>> m_containersToDocuments;
+    };
+}
 
 #endif
