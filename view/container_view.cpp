0978dfe147c8c1c7f32c4a14eca5c724f0aadfdb
LuluEmc2 <lucien.vite@proton.me>
2024-09-27 20:48:35 +0200

    Added container view namespace.

--- view/container_view.cpp
+++ view/container_view.cpp
@@ -1,17 +1,20 @@
 #include <container_view.hpp>
 
-void short_view(tuple<string, string, string> container)
+namespace container_view
 {
-    cout << get<0>(container) << "\n";
-}
-
-void medium_view(tuple<string, string, string> container)
-{
-    cout << get<0>(container) << " : " << get<1>(container) << "\n";
-}
-
-void long_view(tuple<string, string, string> container)
-{
-    cout << get<0>(container) << " : " << get<1>(container) << " : \n\n\t"
-    << get<2>(container) << "\n";
+    void short_view(std::tuple<std::string, std::string, std::string> container)
+    {
+        std::cout << std::get<0>(container) << "\n";
+    }
+    
+    void medium_view(std::tuple<std::string, std::string, std::string> container)
+    {
+        std::cout << std::get<0>(container) << " : " << std::get<1>(container) << "\n";
+    }
+    
+    void long_view(std::tuple<std::string, std::string, std::string> container)
+    {
+        std::cout << std::get<0>(container) << " : " << std::get<1>(container) << " : \n\n\t"
+        << std::get<2>(container) << "\n";
+    }
 }
--- view/container_view.hpp
+++ view/container_view.hpp
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <tuple>
 
-using namespace std;
-
-//Only id, arguments : a tuple of id, name, description
-void short_view(tuple<string, string, string> container);
-//Id and title, arguments : a tuple of id, name, description
-void medium_view(tuple<string, string, string> container);
-//Everything, arguments : a tuple of id, name, description
-void long_view(tuple<string, string, string> container);
+namespace container_view
+{
+    //Only id, arguments : a tuple of id, name, description
+    void short_view(std::tuple<std::string, std::string, std::string> container);
+    //Id and title, arguments : a tuple of id, name, description
+    void medium_view(std::tuple<std::string, std::string, std::string> container);
+    //Everything, arguments : a tuple of id, name, description
+    void long_view(std::tuple<std::string, std::string, std::string> container);
+}
 
 #endif
