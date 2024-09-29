46774502768d2eeab2205316d9aedefef9ad3102
LuluEmc2 <lucien.vite@proton.me>
2024-09-29 12:30:39 +0200

    Fix : Space were not read.

--- view/utils_view.cpp
+++ view/utils_view.cpp
@@ -28,33 +28,64 @@ namespace utils_view
 
         std::getline(std::cin, input);
 
-        if (std::is_same<T, char>::value)
-        {
-            if (input.empty() || input.length() != 1) {
-                std::cout << INVALID_ENTRY_ERROR;
-                clear_input();
-                goto read_input;
-            }
-            value = input[0];
-        }
-        else
-        {
-            std::stringstream ss(input);
-            ss >> value;
+        std::stringstream ss(input);
+        ss >> value;
         
-            if (ss.fail() || !ss.eof()) {
-                std::cout << INVALID_ENTRY_ERROR;
-                clear_input();
-                goto read_input;
-            }
+        if (ss.fail() || !ss.eof()) 
+        {
+            std::cout << INVALID_ENTRY_ERROR;
+            clear_input();
+            goto read_input;
         }
+        
+
+        return value;
+    }
+
+    template <> char read_input(std::string _message)
+    {
+    read_input:
+        std::cout << _message;
+
+        char value;
+        std::string input;
+
+        std::getline(std::cin, input);
+
+        if (input.empty() || input.length() != 1) {
+            std::cout << INVALID_ENTRY_ERROR;
+            clear_input();
+            goto read_input;
+        }
+
+        value = input[0];
+
+        return value;
+    }
+
+    template <> std::string read_input(std::string _message)
+    {
+    read_input:
+        std::cout << _message;
+
+        std::string value;
+        std::string input;
+
+        std::getline(std::cin, input);
+        
+        if (input.empty())
+        {
+            std::cout << INVALID_ENTRY_ERROR;
+            clear_input();
+            goto read_input;
+        }
+        
+        value = input;
 
         return value;
     }
 
     template int read_input<int>(std::string _message);
     template double read_input<double>(std::string _message);
-    template char read_input<char>(std::string _message);
     template long read_input<long>(std::string _message);
-    template std::string read_input<std::string>(std::string _message);
 }
