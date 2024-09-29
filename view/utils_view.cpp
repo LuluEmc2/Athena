f9e36169e2de5768392d1b7716c8cea11a2d0cd5
LuluEmc2 <lucien.vite@proton.me>
2024-09-29 10:52:59 +0200

    Added read_input method.

--- view/utils_view.cpp
+++ view/utils_view.cpp
@@ -11,4 +11,50 @@ namespace utils_view
 
         std::cout << "\n";
     }
+
+    void clear_input()
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    template <typename T> T read_input(std::string _message)
+    {
+    read_input:
+        std::cout << _message;
+
+        T value;
+        std::string input;
+
+        std::getline(std::cin, input);
+
+        if (std::is_same<T, char>::value)
+        {
+            if (input.empty() || input.length() != 1) {
+                std::cout << INVALID_ENTRY_ERROR;
+                clear_input();
+                goto read_input;
+            }
+            value = input[0];
+        }
+        else
+        {
+            std::stringstream ss(input);
+            ss >> value;
+        
+            if (ss.fail() || !ss.eof()) {
+                std::cout << INVALID_ENTRY_ERROR;
+                clear_input();
+                goto read_input;
+            }
+        }
+
+        return value;
+    }
+
+    template int read_input<int>(std::string _message);
+    template double read_input<double>(std::string _message);
+    template char read_input<char>(std::string _message);
+    template long read_input<long>(std::string _message);
+    template std::string read_input<std::string>(std::string _message);
 }
--- view/utils_view.hpp
+++ view/utils_view.hpp
@@ -2,11 +2,17 @@
 #define _UTILS_VIEW_
 
 #include <iostream>
+#include <sstream>
+#include <limits>
+
+#define INVALID_ENTRY_ERROR "Not a valid entry."
 
 namespace utils_view
 {
     //Draws a line of the length and char given in params
     void line(int _length, std::string _patern);
+    template <typename T>
+    T read_input(std::string _message);
 }
 
 #endif
